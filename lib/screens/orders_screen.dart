c4778d854a480b6c1d954f35e3611489e030919a
coding-96 <yousif.coding96@gmail.com>
2022-10-12 18:42:39 +0300

    orders finish

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/orders.dart' show Orders;
+import '../widgets/order_item.dart';
 
 class OrderScreen extends StatelessWidget {
   const OrderScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final ordersData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, i) => OrderItem(ordersData.orders[i]),
+        itemCount: ordersData.orders.length,
       ),
     );
   }
